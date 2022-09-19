1e6d2fb601e5c42062a84432786c24286e247084
unknown <ignacioduarte099@gmail.com>
2022-09-19 10:05:50 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SynopsisComponent } from './synopsis/synopsis.component';
+import { CharactersComponent } from './characters/characters.component';
+import { MonstersComponent } from './monsters/monsters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SynopsisComponent,
+    CharactersComponent,
+    MonstersComponent
   ],
   imports: [
     BrowserModule,
